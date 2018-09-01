105029bd3d1776d1ea8ec9371c5403e5ca1f96a2
Aleksandra Niewiera <218427@student.pwr.edu.pl>
2018-09-01 14:21:30 +0200

    Weather menu and searchbar connected with Weather Board

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,39 @@
 import './SearchBar_styles.css';
+import React from 'react';
 
+export default class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '' };
+    this.onContentChange = this.onContentChange.bind(this);
+    this.onEnterPressed = this.onEnterPressed.bind(this);
+  }
+
+  onContentChange(event) {
+    console.log(event.target.value);
+    this.setState({ text: event.target.value });
+  }
+
+  onEnterPressed(event) {
+    if (event.key === 'Enter') {
+      this.props.cityName(this.state.text);
+      this.setState({ text: '' });
+      //dodaj text z search box do listy i zrob update requestu z tym tekstem
+      console.log(event.target.value);
+    }
+  }
+
+  render() {
+    return (
+      <input
+        id="search-bar"
+        placeholder="Enter city name"
+        value={this.state.text}
+        onChange={this.onContentChange}
+        onKeyPress={this.onEnterPressed}
+      />
+    );
+  }
+}
 
 // wyszukiwarka
--- src/views/App/index.jsx
+++ src/views/App/index.jsx
@@ -2,41 +2,48 @@ import React, { PureComponent, Fragment } from 'react';
 import axios from 'axios';
 import {workWithResponse} from './Request';
 import WeatherBoard from '../../components/WeatherBoard/WeatherBoard';
+import WeatherMenu from '../../components/WeatherMenu/WeatherMenu';
 
-
+const API_KEY = '436d1eea572e5d346f8cd5eb7c8dfa14';
 
 class App extends PureComponent {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       searchCity: 'Warszawa',
-      response: null
-    }
+      response: null,
+    };
+
+    this.doRequest = this.doRequest.bind(this);
   }
 
-  doRequest(){
-    const request = `http://api.openweathermap.org/data/2.5/forecast?q=${this.state.searchCity}&mode=json&units=metric&APPID=436d1eea572e5d346f8cd5eb7c8dfa14`;
-    axios.get(request)
-          .then(res => {
-            const response = workWithResponse(res.data, this.state.searchCity);
-            this.setState({ response: response });
-          })
+  doRequest() {
+    const request = `http://api.openweathermap.org/data/2.5/forecast?q=${
+      this.state.searchCity
+    }&mode=json&units=metric&APPID=${API_KEY}`;
+    axios.get(request).then(res => {
+      const response = workWithResponse(res.data, this.state.searchCity);
+      this.setState({ response: response });
+    });
   }
 
-  componentWillMount(){
+  UNSAFE_componentWillMount() {
     this.doRequest();
   }
 
   render() {
-
-    if(!this.state.response){
-      return <div></div>;
+    if (!this.state.response) {
+      return <div />;
     }
 
-    return (<Fragment>
-        <WeatherBoard weatherList = {this.state.response} />
-    </Fragment>);
+    return <Fragment>
+        <WeatherMenu
+          searchCity={this.state.searchCity}
+          updateSearchCity={city => this.setState({ searchCity: city }, this.doRequest)}
+          refresh={this.doRequest}
+        />
+        <WeatherBoard weatherList={this.state.response} />
+      </Fragment>;
   }
 }
 
