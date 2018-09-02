4c3378b66392323333548c1b14747d537cc5dde0
Michał Stojke <m.stojke.glg@gmail.com>
2018-09-02 20:58:30 +0200

    Update StyleList_styles.js
    Added new styles

--- src/components/CityList/CityList_styles.js
+++ src/components/CityList/CityList_styles.js
@@ -1,17 +1,43 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  margin: var(--space-lg);
   font-size: var(--font-xl);
-  color: ${props => (props.color === 'default' ? 'var(--text1)' : 'var(--primary2)')};
   width: 300px;
+  height: 441px;
   position: absolute;
   top: 0;
   right: 0;
+  background-color: rgba(255, 255, 255, 0.4);
+  margin: 20px;
+  border: 3px solid #0097d6;
+  border-radius: 15px;
+  overflow-y: scroll;
+
+  ::-webkit-scrollbar {
+    width: 15px;
+  }
+  ::-webkit-scrollbar-track {
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar-thumb {
+    height: 10px;
+    border: 5px solid rgba(0, 0, 0, 0);
+    background-clip: padding-box;
+    background-color: rgba(0, 0, 0, 0.15);
+    border-radius: 10px;
+  }
+  ::-webkit-scrollbar-thumb:hover {
+    height: 10px;
+    border: 5px solid rgba(0, 0, 0, 0);
+    background-clip: padding-box;
+    border-radius: 10px;
+    background-color: #555;
+  }
 `;
 
 export const UnorderedList = styled.ul`
   cursor: pointer;
+  padding: 20px;
 `;
 
 export const ListItem = styled.li`
@@ -19,14 +45,17 @@ export const ListItem = styled.li`
   justify-content: flex-end;
   grid-gap: 10px;
   grid-template-columns: 2fr 25px 25px;
-  grid-template-rows: 25px;
+  grid-template-rows: 35px;
   grid-template-areas: 'city favbtn delbtn';
+  &.selected {
+    color: var(--primary3);
+  }
 `;
 
 export const City = styled.div`
   grid-area: city;
   &:hover {
-    color: var(--grey1);
+    color: var(--primary3);
   }
 `;
 
